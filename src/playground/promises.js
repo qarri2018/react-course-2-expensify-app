853617f4170c8fb41f7d7fa4ec72dd5c568ffbe8
qarri2018 <karri.asikainen@outlook.com>
2019-02-04 15:05:03 +0200

    safd

--- src/playground/promises.js
+++ src/playground/promises.js
@@ -12,10 +12,11 @@ console.log('before');
 
 promise.then((data) => {
   console.log('1', data);
-
+  
   return new Promise((resolve, reject) => {
     setTimeout(() => {
       resolve('This is my other promise');
+      // reject('Something went wrong!');
     }, 5000);
   });
 }).then((str) => {
