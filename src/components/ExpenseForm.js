3507bcd41ab9166c981f08cb9354c429cf20fc2b
qarri2018 <44335722+qarri2018@users.noreply.github.com>
2019-02-23 14:58:10 +0200

    Styled header, page header and filters

--- src/components/ExpenseForm.js
+++ src/components/ExpenseForm.js
@@ -62,16 +62,19 @@ export default class ExpenseForm extends React.Component {
             type="text"
             placeholder="Description"
             autoFocus
+            className="text-input"
             value={this.state.description}
             onChange={this.onDescriptionChange}
           />
           <input
             type="text"
             placeholder="Amount"
+            className="text-input"
             value={this.state.amount}
             onChange={this.onAmountChange}
           />
           <SingleDatePicker
+            className="text-input"
             date={this.state.createdAt}
             onDateChange={this.onDateChange}
             focused={this.state.calendarFocused}
@@ -81,6 +84,7 @@ export default class ExpenseForm extends React.Component {
           />
           <textarea
             placeholder="Add a note for your expense (optional)"
+            className="textarea"
             value={this.state.note}
             onChange={this.onNoteChange}
           >
